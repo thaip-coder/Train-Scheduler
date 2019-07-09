7f16720581bc8e13e09271abd3a210c4f9b31b6e
Thai Phan <thaiphan90@yahoo.com>
2019-07-08 19:39:31 -0500

    updated javascript

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -16,7 +16,38 @@ var database = firebase.database();
 var train = "";
 var destination = "";
 var frequency = 0;
-
-/* ---------- Functions ----------*/
+var firstTrain = "";
 
 /* ---------- Processes ---------- */
+$('#add-train').on('click', function(event) {
+    event.preventDefault();
+
+    train = $('#add-name').val().trim();
+    destination = $('#add-destination').val().trim();
+    frequency = $('#add-frequency').val().trim();
+    firstTrain = $('#add-time').val().trim();
+
+    database.ref("/trains").push({
+        train: train,
+        destination: destination,
+        frequency: frequency,
+        firstTrain: firstTrain
+    });
+});
+
+database.ref("/trains").on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+    console.log(snapshot.val(),train);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().frequency);
+    console.log(snapshot.val().firstTrain);
+
+    train = snapshot.val().train;
+    destination = snapshot.val().destination;
+    frequency = snapshot.val().frequency;
+    firstTrain = snapshot.val().firstTrain;
+
+    $('#trains').append("<tr><td>" + train + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + firstTrain + "</td><td>" + "Minutes Away" + "</td>");
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
